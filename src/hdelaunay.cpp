21bb48e89c0766507659b6afc07d193f709d1bdc
stla <laurent_step@yahoo.fr>
2022-05-10 16:00:36 +0200

    hdelaunay cpp test

--- src/hdelaunay.cpp
+++ src/hdelaunay.cpp
@@ -7,21 +7,24 @@
 
 typedef CGAL::Hyperbolic_Delaunay_triangulation_traits_2<>  HDtt;
 typedef HDtt::Point_2                                         HPoint;
-typedef CGAL::Hyperbolic_Delaunay_triangulation_2<HDtt>       HDt;
+typedef CGAL::Hyperbolic_Delaunay_triangulation_2<HDtt, Tds2>       HDt;
 
 size_t htest(std::vector<std::vector<double>> points){
-	std::vector<HPoint> hpts;
-	const size_t npoints = points.size();
+
+	std::vector<std::pair<HPoint, unsigned>> hpts;
+
+	//std::vector<HPoint> hpts;
+	const unsigned npoints = points.size();
 	hpts.reserve(npoints);
-	for(size_t i=0; i != npoints; i++){
+	for(unsigned i=0; i != npoints; i++){
 		const std::vector<double> pt = points[i];
-		hpts[i] = HPoint(pt[1], pt[2]);
+		hpts.emplace_back(std::make_pair(HPoint(pt[0], pt[1]), i);
 	}
 	HDt hdt;
 	hdt.insert(hpts.begin(), hpts.end());
 	for(HDt::All_edges_iterator ed = hdt.all_edges_begin(); ed != hdt.all_edges_end(); ++ed){
 		HDt::Vertex_handle sVertex = ed->first->vertex(HDt::cw(ed->second));
-		Rcpp::Rcout << sVertex->vertex;
+		Rcpp::Rcout << sVertex->info();
 	}
 	return hdt.number_of_vertices();
 }
