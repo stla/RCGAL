c83c189b34dd3f16eb27d8ce4bab61a80e9c2244
stla <laurent_step@yahoo.fr>
2022-05-10 16:29:37 +0200

    hdelaunay cpp test

--- src/hdelaunay.cpp
+++ src/hdelaunay.cpp
@@ -5,27 +5,30 @@
 #include <CGAL/Hyperbolic_Delaunay_triangulation_2.h>
 #include <CGAL/Hyperbolic_Delaunay_triangulation_traits_2.h>
 
-typedef CGAL::Hyperbolic_Delaunay_triangulation_traits_2<>  HDtt;
-typedef HDtt::Point_2                                         HPoint;
-typedef CGAL::Triangulation_data_structure_2<CGAL::Triangulation_vertex_base_with_info_2<unsigned, HDtt>, CGAL::Hyperbolic_triangulation_face_base_2<HDtt>> HTds;
-typedef CGAL::Hyperbolic_Delaunay_triangulation_2<HDtt, HTds>       HDt;
+typedef CGAL::Hyperbolic_Delaunay_triangulation_traits_2<K> HDtt;
+typedef HDtt::Point_2 HPoint;
+typedef CGAL::Triangulation_data_structure_2<
+    CGAL::Triangulation_vertex_base_with_info_2<unsigned, HDtt>,
+    CGAL::Hyperbolic_triangulation_face_base_2<HDtt>>
+    HTds;
+typedef CGAL::Hyperbolic_Delaunay_triangulation_2<HDtt, HTds> HDt;
 
-size_t htest(std::vector<std::vector<double>> points){
+size_t htest(std::vector<std::vector<double>> points) {
+  std::vector<std::pair<HPoint, unsigned>> hpts;
 
-	std::vector<std::pair<HPoint, unsigned>> hpts;
-
-	//std::vector<HPoint> hpts;
-	const unsigned npoints = points.size();
-	hpts.reserve(npoints);
-	for(unsigned i=0; i != npoints; i++){
-		const std::vector<double> pt = points[i];
-		hpts.emplace_back(std::make_pair(HPoint(pt[0], pt[1]), i));
-	}
-	HDt hdt;
-	hdt.insert(hpts.begin(), hpts.end());
-	for(HDt::All_edges_iterator ed = hdt.all_edges_begin(); ed != hdt.all_edges_end(); ++ed){
-		HDt::Vertex_handle sVertex = ed->first->vertex(HDt::cw(ed->second));
-		Rcpp::Rcout << sVertex->info();
-	}
-	return hdt.number_of_vertices();
+  // std::vector<HPoint> hpts;
+  const unsigned npoints = points.size();
+  hpts.reserve(npoints);
+  for(unsigned i = 0; i != npoints; i++) {
+    const std::vector<double> pt = points[i];
+    hpts.emplace_back(std::make_pair(HPoint(pt[0], pt[1]), i));
+  }
+  HDt hdt;
+  hdt.insert(hpts.begin(), hpts.end());
+  for(HDt::All_edges_iterator ed = hdt.all_edges_begin();
+      ed != hdt.all_edges_end(); ++ed) {
+    HDt::Vertex_handle sVertex = ed->first->vertex(HDt::cw(ed->second));
+    Rcpp::Rcout << sVertex->info();
+  }
+  return hdt.number_of_vertices();
 }
