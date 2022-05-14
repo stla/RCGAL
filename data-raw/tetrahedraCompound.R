ecd6249997ff3b3454757162e5bc1d78b9037b61
stla <laurent_step@yahoo.fr>
2022-05-14 02:02:45 +0200

    fixed help Mesh

--- data-raw/tetrahedraCompound.R
+++ data-raw/tetrahedraCompound.R
@@ -1,7 +1,9 @@
-library(gmp)
+# the compound of five tetrahedra ####
+library(gmp) # we use rational numbers for the vertex coordinates
 library(RCGAL)
 library(rgl)
 
+# all vertices
 Vertices <- function(a, b, c, phi, O){
   t(cbind(
     c( a,  a,  a),
@@ -27,22 +29,25 @@ Vertices <- function(a, b, c, phi, O){
   ))
 }
 
-phi <- (1+sqrt(5))/2
+# 'double' vertex coordinates
+phi <- (1 + sqrt(5)) / 2
 a <- 1 / sqrt(3)
 b <- a / phi
 c <- a * phi
 O <- 0
 vertices <- Vertices(a, b, c, phi, O)
 
+# 'gmp' vertex coordinates
 phi <- qsqrtPhi(17)
-a <- 1L/qsqrt3(22)
+a <- 1L / qsqrt3(22)
 b <- a / phi
 c <- a * phi
 O <- as.bigq(0)
 gmpVertices <- Vertices(a, b, c, phi, O)
-# check:
-asNumeric(gmpVertices) - vertices
+# check the difference is close to 0:
+max(abs(asNumeric(gmpVertices) - vertices))
 
+# the face indices are common to all tetrahedra
 faces <- rbind(
   c(1L, 2L, 3L),
   c(3L, 2L, 4L),
@@ -50,6 +55,7 @@ faces <- rbind(
   c(1L, 3L, 4L)
 )
 
+# define the five tetrahedra meshes
 mesh1 <- list(
   "vertices" = vertices[c(17, 14, 2, 11), ],
   "faces" = faces
@@ -71,61 +77,98 @@ mesh5 <- list(
   "faces" = faces
 )
 
+# put them in a list to apply the intersection algorithm
 meshes <- list(
   mesh1, mesh2, mesh3, mesh4, mesh5
 )
 
 tetrahedraCompound <- list(
+
   "meshes" = lapply(meshes, function(mesh){
     list("vertices" = as.bigq(mesh[["vertices"]]), faces = faces)
   }),
+
   "rglmeshes" = list(
+
     tmesh3d(
-      "vertices" = t(mesh1[["vertices"]]),
-      "indices" = t(faces),
+      "vertices"    = t(mesh1[["vertices"]]),
+      "indices"     = t(faces),
       "homogeneous" = FALSE
     ),
     tmesh3d(
-      "vertices" = t(mesh2[["vertices"]]),
-      "indices" = t(faces),
+      "vertices"    = t(mesh2[["vertices"]]),
+      "indices"     = t(faces),
       "homogeneous" = FALSE
     ),
     tmesh3d(
-      "vertices" = t(mesh3[["vertices"]]),
-      "indices" = t(faces),
+      "vertices"    = t(mesh3[["vertices"]]),
+      "indices"     = t(faces),
       "homogeneous" = FALSE
     ),
     tmesh3d(
-      "vertices" = t(mesh4[["vertices"]]),
-      "indices" = t(faces),
+      "vertices"    = t(mesh4[["vertices"]]),
+      "indices"     = t(faces),
       "homogeneous" = FALSE
     ),
     tmesh3d(
-      "vertices" = t(mesh5[["vertices"]]),
-      "indices" = t(faces),
+      "vertices"    = t(mesh5[["vertices"]]),
+      "indices"     = t(faces),
       "homogeneous" = FALSE
     )
   )
 )
 
+# compute the intersection of the 'gmp' meshes
+inter <- MeshesIntersection(
+  tetrahedraCompound[["meshes"]], numbersType = "gmp", merge = TRUE
+)
 
-
-inter <- MeshesIntersection(tetrahedraCompound$meshes, numbersType = "gmp")
-
-open3d(windowRect = c(50, 50, 562, 562))
+# plot
+open3d(windowRect = c(50, 50, 562, 562), zoom = 0.75)
 bg3d("#363940")
+# first the five tetrahedra with transparency
 invisible(lapply(
   tetrahedraCompound[["rglmeshes"]], shade3d,
-  color = "yellow", alpha = 0.2
+  color = "yellow", alpha = 0.1
 ))
+# now the intersection
 rglinter <- tmesh3d(
-  "vertices" = t(inter[["vertices"]]),
-  "indices" = t(inter[["faces"]]),
+  "vertices"    = t(inter[["vertices"]]),
+  "indices"     = t(inter[["faces"]]),
   "homogeneous" = FALSE
 )
-open3d(windowRect = c(50, 50, 562, 562), zoom = 0.9)
-shade3d(rglinter, color = "gold")
+shade3d(rglinter, color = "gainsboro")
+# and finally the edges
 plotEdges(
   inter[["vertices"]], inter[["exteriorEdges"]],
-  only = inter[["exteriorVertices"]], color = "navy"
+  only = inter[["exteriorVertices"]], color = "darkmagenta"
+)
+
+# we can recognize some exact values in the 'gmpVertices', for instance
+#   the value at entry \code{(1, 1)} is \code{1/sqrt(3)}:
+asNumeric(1L / gmpVertices[1L, 1L]^2L)
+
+
+library(microbenchmark)
+
+tetrahedraCompound <- list(
+
+  "double_meshes" = lapply(meshes, function(mesh){
+    list("vertices" = mesh[["vertices"]], faces = faces)
+  }),
+
+  "gmp_meshes" = lapply(meshes, function(mesh){
+    list("vertices" = as.bigq(mesh[["vertices"]]), faces = faces)
+  })
+
+)
+
+microbenchmark(
+  exactKernel = MeshesIntersection(
+    tetrahedraCompound[["double_meshes"]], numbersType = "lazyExact"
+  ),
+  gmpKernel = MeshesIntersection(
+    tetrahedraCompound[["gmp_meshes"]], numbersType = "gmp"
+  ),
+  times = 5
 )
